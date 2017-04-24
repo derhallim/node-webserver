b8bb0664f459566c4d4e64d6bef5a2065c6ce39b
Mohamed Derhalli <derhallim@gmail.com>
2017-04-24 13:35:56 -0400

    added projects and modified navigation

--- server.js
+++ server.js
@@ -38,6 +38,13 @@ app.get('/', (req, res) => {
     })
 });
 
+
+app.get('/projects', (req, res) =>{
+    res.render('projects.hbs', {
+        pageTitle: 'My projects'
+    })
+});
+
 app.get('/about', (req, res) => {
     res.render('about.hbs', {
         pageTitle: 'About page', 
